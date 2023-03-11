b3eeeb8b2333f34639a12999573c5e2f8329f48f
JAYUN <s2115@e-mirim.hs.kr>
2023-03-12 07:06:54 +0900

    object

--- ex01.js
+++ ex01.js
@@ -48,3 +48,24 @@ function scopeTest(someVal) {
 }
 scopeTest(true);
 
+console.log("---------------------------------------");
+
+const cathy = {
+  "name" : "cathy",
+  "age" : 19,
+  "skills" : ["자바스크립트", "파이썬", "코볼"]
+};
+
+// { name: 'cathy', age: 19, skills: [ '자바스크립트', '파이썬', '코볼' ] }
+console.log(cathy)
+// cathy
+console.log(cathy.name);
+console.log(cathy["name"]);
+
+cathy.city = "Seoul";
+// { name: 'cathy', age: 19, skills: [ '자바스크립트', '파이썬', '코볼' ], city: 'Seoul'}
+console.log(cathy);
+
+delete cathy.city;
+// { name: 'cathy', age: 19, skills: [ '자바스크립트', '파이썬', '코볼' ] }
+console.log(cathy);
