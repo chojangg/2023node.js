3ece2cf6cc7e0de7fcade09a267fbdd7899c7491
JAYUN <s2115@e-mirim.hs.kr>
2023-03-12 07:05:39 +0900

    범위(scope)의 이해

--- ex01.js
+++ ex01.js
@@ -3,6 +3,8 @@ console.log("node.js 실행하기");
 console.log('cmd에서 node '+파일명+'을 입력해봐요~!');
 console.log(`cmd에서 node ${파일명}을 입력해봐요~!`);
 
+console.log("---------------------------------------");
+
 // 일반형
 // - (매개변수) => {함수코드}
 func_arrow1 = (str) => {
@@ -22,3 +24,27 @@ console.log(func_arrow2('fa2'));
 console.log(func_arrow3('fa3'));
 /* arrow function과 function 의 차이점은 
 this에서 차이가 있으므로, 그대로 대체할 수는 없다 */
+
+console.log("---------------------------------------");
+
+function scopeTest(someVal) {
+  // 정의 되지 않은 변수 typeof
+  console.log("1 : ", typeof test_none);
+  // 함수범위 테스트 (곧 블럭안에서 정의 될 변수)
+  console.log("2-1 : ", typeof test_in_var);
+  console.log("2-2 : ", typeof test_in_let);
+
+  if(someVal) {
+      // 블럭단위 함수
+      console.log("3-1 : ", typeof test_in_var);
+      // var의 경우에는 호이스팅 될 때 undefined로 초기화
+      // console.log("3-2 : ", typeof test_in_let);
+      // let의 경우에는 일시적 사각지대(TDZ : Temporal Dead Zone)에 놓여진다
+
+      var test_in_var;
+      let test_in_let;
+      console.log("3-3 : ", typeof test_in_let); // undefined
+  }
+}
+scopeTest(true);
+
