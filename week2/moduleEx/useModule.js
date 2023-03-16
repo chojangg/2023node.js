9c1c96f8d367bf15ff6e53f18c152d40f7eb60be
chojangg <s2115@e-mirim.hs.kr>
2023-03-17 08:52:40 +0900

    module

---
+++ week2/moduleEx/useModule.js
@@ -0,0 +1,11 @@
+const { text_odd, text_even} = require("./moduleText");
+const checkNumberFunc = require("./moduleFunc");
+
+function checkStringOddorEven(str) {
+    if(str.length % 2){
+        return text_odd;
+    }
+    return text_even;
+}
+console.log("useModule : ", checkNumberFunc(10));
+console.log("useModule : ", checkStringOddorEven('Hello'));
