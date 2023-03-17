524440e02c5b0be0f16bc7ca52e6e6c43a599e27
chojangg <s2115@e-mirim.hs.kr>
2023-03-17 10:04:48 +0900

    내장객체

--- week2/moduleEx/moduleText.js
+++ week2/moduleEx/moduleText.js
@@ -1,9 +1,23 @@
 const text_odd = "홀수입니다";
 const text_even = "짝수입니다";
 
-console.log("1 : ");
+console.log("1 : ", exports === module.exports);
+console.log("2 : ", exports, module.exports);
 
+/*
 module.exports = {
     text_odd,
     text_even,
 }
+*/
+exports.text_odd = text_odd;
+exports.text_even = text_even;
+
+
+/*
+// 이건 안된다
+exports = {
+    text_odd,
+    text_even,
+}
+*/
