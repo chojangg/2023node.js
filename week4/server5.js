eda878e865c69a57e076013de3c40e92b3d20899
JAYUN <s2115@e-mirim.hs.kr>
2023-03-31 10:20:03 +0900

    메뉴 불러오기

--- week4/server5.js
+++ week4/server5.js
@@ -32,6 +32,14 @@ const server = http.createServer(async (req, res)=> {
         console.log("log",fileListText);
         fileListText += '</ul>';
 
+        const searchParams = new URL(req.url, "http://localhost:8089").searchParams;
+        console.log("searchParms", searchParams);
+
+        const param_date = searchParams.get("date") || "null";
+        
+        const fileName = path.join(__dirname, `./textFile/menu_${param_date}.txt`);
+        let fileData = await fs.readFile(fileName);
+
         const template = `
         <!DOCTYPE html>
         <html lang="en">
@@ -42,6 +50,8 @@ const server = http.createServer(async (req, res)=> {
             <body>
                 <h1><a href="/">급식 메뉴</a></h1>
                 ${fileListText}
+                <br>
+                ${fileData}
             </body>
         </html>
         `
