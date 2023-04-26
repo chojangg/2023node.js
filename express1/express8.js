1a03ef86ba8ea26905aa88fcf5408c6dbbbfbb2b
JAYUN <s2115@e-mirim.hs.kr>
2023-04-26 09:18:08 +0900

    morgan

---
+++ express1/express8.js
@@ -0,0 +1,16 @@
+// static 미들웨어
+// 이미지파일, 자바스크립트파일, css파일 등
+
+const express = require('express');
+
+const app = express();
+app.use(express.static(__dirname+'/public'));
+
+app.use(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end("<img src = '/salmon.jpg' width='100%'>");
+});
+
+app.listen(8889, function(){
+    console.log("8889 포트입니다");
+});
